fe608d31f45f835ff245fe7f039c9c5a2167a3b7
Lee Marsden <marzy@Emmas-iPhone.home>
2016-05-15 21:35:35 +0100

    Added latest

--- gulp/config/revall.js
+++ gulp/config/revall.js
@@ -4,7 +4,7 @@ var path = require('./_global').paths;
 
 module.exports = {
   src: path.dest + '**',
-  dontRenameFile: [/^\/favicon.ico$/g, '.html', '.xml'],
-  dontUpdateReference: ['.html'],
+  dontRenameFile: [/^\/favicon.ico$/g, '.html', '.xml', 'logo.png', '1.png'],
+  dontUpdateReference: ['.html', 'logo.png', '1.png'],
   dest: path.prod,
 }
