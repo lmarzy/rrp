4a69a01f382550891d0b354c9f6f0df9f2448046
lmarzy <me@phz3.net>
2018-02-13 20:59:59 +0000

    Updates to NeoGuard RIGID for sale button. Added sales of punctureguard and updated sale page with zip ties

--- src/browserify/components/update-size.js
+++ src/browserify/components/update-size.js
@@ -3,8 +3,12 @@
 */
 
 $('#js-productSizes').change(function() {
-  var size = $(this).val();
-  var button = $('#js-forSaleBtn');
+  var me = $(this);
 
+  var size = me.val();
+  var id = me.find('option:selected').data('id');
+  var button = $('#js-forSaleBtn');
+  
   button.data('item-custom1-value', size);
+  button.attr('data-item-id', id);
 });
