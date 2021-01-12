3f51b4ef3062b0013fac5b282b62783eba0508c0
lmarzy <me@phz3.net>
2021-01-12 20:24:07 +0000

    Added new compatible fork for proguard bolton

--- src/browserify/components/gallery.js
+++ src/browserify/components/gallery.js
@@ -1,36 +1,35 @@
 /*
  * GALLERY COMPONENT
-*/
+ */
 
-'use strict';
+"use strict";
 
 export const gallery = () => {
+  let gallery = document.getElementsByClassName("c-gallery")[0];
 
-  let gallery   = document.getElementsByClassName('c-gallery')[0];
+  if (!gallery) return;
 
-  if(!gallery) return;
+  let images = gallery.getElementsByClassName("c-gallery__img");
+  let nextBtn = gallery.getElementsByClassName("c-gallery__btn--next")[0];
+  let prevBtn = gallery.getElementsByClassName("c-gallery__btn--prev")[0];
+  let counter = 0;
 
-  let images    = gallery.getElementsByTagName('img');
-  let nextBtn   = gallery.getElementsByClassName('c-gallery__btn--next')[0];
-  let prevBtn   = gallery.getElementsByClassName('c-gallery__btn--prev')[0];
-  let counter   = 0;
-
-  function showImage (index) {
+  function showImage(index) {
     // Set classname on the image-elements (hide them)
     for (var i = 0; i < images.length; i += 1) {
-      images[i].classList.remove('is-visible');
+      images[i].classList.remove("is-visible");
     }
 
     // Add the showImage classname to the img-element you want
     // images[index].classList.remove('is-hidden');
-    images[index].classList.add('is-visible');;
+    images[index].classList.add("is-visible");
   }
 
-  function nextImg () {
+  function nextImg() {
     // counter variable gets the current img selected
     // if we have the last img, we switch back to the first one again
 
-    if (counter < images.length -1) {
+    if (counter < images.length - 1) {
       counter += 1;
     } else {
       counter = 0;
@@ -39,7 +38,7 @@ export const gallery = () => {
     showImage(counter);
   }
 
-  function prevImg () {
+  function prevImg() {
     // counter variable gets the current img selected
     // if we have the first img, we switch back to the last one again
 
@@ -52,9 +51,8 @@ export const gallery = () => {
     showImage(counter);
   }
 
-  nextBtn.addEventListener('click', nextImg);
-  prevBtn.addEventListener('click', prevImg);
+  nextBtn.addEventListener("click", nextImg);
+  prevBtn.addEventListener("click", prevImg);
 
   showImage(0);
-
-}
+};
--- src/browserify/components/product-thumbs.js
+++ src/browserify/components/product-thumbs.js
@@ -1,31 +1,31 @@
 /*
  * Accordion component
-*/
+ */
 
-import { selectorMatches } from '../helpers/selector-matches';
+import { selectorMatches } from "../helpers/selector-matches";
 
 export const productThumbs = (selector) => {
-
   const thumbs = document.querySelector(selector);
 
-  if(!thumbs) return;
+  if (!thumbs) return;
 
-  const gallery     = document.getElementsByClassName('c-gallery')[0];
-  const images      = gallery.getElementsByTagName('img');
+  const gallery = document.getElementsByClassName("c-gallery")[0];
+  const images = gallery.getElementsByClassName("c-gallery__img");
 
   const showImage = (e) => {
-    if (!e.target || !selectorMatches(e.target, 'img')) return;
+    if (!e.target || !selectorMatches(e.target, "img")) return;
 
     e.preventDefault();
 
     for (var i = 0; i < images.length; i += 1) {
-      images[i].classList.remove('is-visible');
+      images[i].classList.remove("is-visible");
     }
 
-    const target = document.getElementById(e.target.getAttribute('data-target'));
-    target.classList.add('is-visible');
-  }
+    const target = document.getElementById(
+      e.target.getAttribute("data-target")
+    );
+    target.classList.add("is-visible");
+  };
 
-  thumbs.addEventListener('click', showImage);
-
-}
+  thumbs.addEventListener("click", showImage);
+};
